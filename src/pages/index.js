96116ff64189e5e2e829dc4db2c0f1eb8e08ba73
idrees66 <idrees0066@gmail.com>
2023-02-05 06:45:33 +0500

    screenshots updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import * as React from "react"
 import Layout from "../components/Layout/Layout"
-import Sidebar from "../components/Sidebar"
+
 
 
 const IndexPage = () => {
   return (
     <Layout />
-    // <Sidebar />
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
